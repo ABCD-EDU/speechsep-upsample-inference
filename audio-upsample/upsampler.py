ee8a88c46c3e8d8e0dd4c9c5fa46db4ebbb2201e
Christian Bayquen <cgabayquen@gmail.com>
2023-04-21 23:03:35 +0800

    add: script for bulk upsampling

---
+++ audio-upsample/upsampler.py
@@ -0,0 +1,30 @@
+import sampling
+import importlib
+importlib.reload(sampling)
+from sampling import run
+from glob import glob
+import os
+from datetime import datetime
+
+# absolute path pls
+directory = "C:/Users/chris/Music/audio_results"
+wavs = glob(directory + "/*hat.wav")
+outdir_name = directory + "/upsampled"
+
+if not os.path.exists(outdir_name):
+    os.makedirs(outdir_name)
+
+start_time = datetime.now()
+print("UPSCALING STARTED: ", start_time)
+    
+for i, wav in enumerate(wavs):
+    current_time = datetime.now().strftime("%H:%M:%S %m-%d")
+    print(f"[{i}/{len(wavs)}] | START: {current_time}", end=" | ")
+    file_name = os.path.basename(wav).split(".")[0]
+    run(input_path=wav, output_path=outdir_name + "/" + file_name + "_up" + ".wav")
+    current_time = datetime.now().strftime("%H:%M:%S %m-%d")
+    print(f"ELAPSED: {datetime.now() - start_time} | FILE: {file_name}")
+
+end_time = datetime.now()
+print("UPSCALING ENDED: ", end_time)
+print("TOTAL DURATION:", end_time - start_time)
