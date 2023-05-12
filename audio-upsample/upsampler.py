0b6be94ba8620d50dd530f44d7355ec7924ad55a
Christian Bayquen <cgabayquen@gmail.com>
2023-05-12 23:32:01 +0800

    edit: use tdqm for upsampler

--- audio-upsample/upsampler.py
+++ audio-upsample/upsampler.py
@@ -5,10 +5,11 @@ from main import run
 from glob import glob
 import os
 from datetime import datetime
+from tqdm import tqdm
 
 # absolute path pls
-# directory = "G:\Thesis\Results\\batchsize1_w_noise_speedperturb\sepformer-libri2mix\\1234\\audio_results"
-directory ="../speech-sep-metrics/results/audio_results"
+directory = "C:/Users/chris/Music/standard_model_3_speakers/audio_results"
+# directory ="../speech-sep-metrics/results/audio_results"
 wavs = glob(directory + "/*hat.wav")
 outdir_name = directory + "/upsampled"
 
@@ -17,14 +18,15 @@ if not os.path.exists(outdir_name):
 
 start_time = datetime.now()
 print("UPSCALING STARTED: ", start_time)
+print("TOTAL NUMBER OF FILES:", len(wavs))
 
-for i, wav in enumerate(wavs):
-    current_time = datetime.now().strftime("%H:%M:%S %m-%d")
-    print(f"[{i}/{len(wavs)}] | START: {current_time}", end=" | ")
+for wav in tqdm(wavs, total=len(wavs)):
+    # current_time = datetime.now().strftime("%H:%M:%S %m-%d")
+    # print(f"[{i}/{len(wavs)}] | START: {current_time}", end=" | ")
     file_name = os.path.basename(wav).split(".")[0]
     run(input_path=wav, output_path=outdir_name + "/" + file_name + "_up" + ".wav")
-    current_time = datetime.now().strftime("%H:%M:%S %m-%d")
-    print(f"ELAPSED: {datetime.now() - start_time} | FILE: {file_name}")
+    # current_time = datetime.now().strftime("%H:%M:%S %m-%d")
+    # print(f"ELAPSED: {datetime.now() - start_time} | FILE: {file_name}")
 
 end_time = datetime.now()
 print("UPSCALING ENDED: ", end_time)
